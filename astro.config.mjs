f2b32a99ee3d28cacbe9060901e3b5df9a115f37
initOKI <sodalgom@naver.com>
2023-04-25 02:28:13 +0900

    feat: .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
