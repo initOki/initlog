eda4010d9d8deef896dba3037c89fb0817f84fe6
initOki <sodalgom@naver.com>
2023-12-19 21:44:41 +0900

    class를 이용해서 button 이벤트 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,4 @@ export default defineConfig({
   markdown: {
     drafts: true,
   },
-  experimental: {
-    devOverlay: true,
-  }
 });
---
+++ src/components/Enrollment/server.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+class GetLectureItems {
+  lectureItems: Array<any>;
+  highSchoolLectureItems: Array<any>;
+  middleSchoolLectureItems: Array<any>;
+
+  constructor() {
+    this.lectureItems = [];
+    this.highSchoolLectureItems = [];
+    this.middleSchoolLectureItems = [];
+
+    this.getLectureItems();
+  }
+
+  async getStudentInfo() {
+    try {
+      const response = await axios.post('https://staging.onuii.com/getLectureInfo');
+    } catch (error) {}
+  }
+
+  async getLectureItems() {
+    try {
+      const response = await axios.post('https://staging.onuii.com/Lecture_StoreItems');
+      this.lectureItems = response.data.ONUEI_RES.DATA.RESPONSE.LECTURE_ITEMS;
+      this.lectureFilterByGroupNo();
+    } catch (error) {}
+  }
+
+  lectureFilterByGroupNo() {
+    this.highSchoolLectureItems = this.lectureItems.filter((item) => item.group_No === 65);
+    this.middleSchoolLectureItems = this.lectureItems.filter((item) => item.group_No === 64);
+  }
+}
+
+export default GetLectureItems;
