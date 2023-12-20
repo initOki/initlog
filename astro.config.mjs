43d2c1470d2c704feabad4ec0e9d1edbdfbdc336
initoki <sodalgom@naver.com>
2023-12-21 02:14:45 +0900

    테스트용 수강권 페이지 작업

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,25 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import sitemap from '@astrojs/sitemap';
 
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [react(), mdx(), sitemap({
     canonicalURL: 'https://initlog.vercel.app'
-  })],
+  }), sentry(), spotlightjs()],
   rendererOptions: {
     styleUrl: false
   },
   renderer: ["@astrojs/renderer-stitches"],
-  server: { port: 3200 },
-  markdown: {
-    drafts: true,
+  server: {
+    port: 3200
   },
+  markdown: {
+    drafts: true
+  }
 });
