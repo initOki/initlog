357cd8a07cf8cd83c8aa16b96ee95ebf665c9730
initOki <sodalgom@naver.com>
2024-02-11 02:24:21 +0900

    빌드 에러 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
-import sentry from "@sentry/astro";
+// import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
