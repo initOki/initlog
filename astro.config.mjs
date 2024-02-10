15a6b62a433b7977755fa4488da6cb126ebd4934
initOki <sodalgom@naver.com>
2024-02-11 01:52:54 +0900

    빌드 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import spotlightjs from "@spotlightjs/astro";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), sentry(), spotlightjs()],
+  integrations: [mdx(), sitemap(), tailwind(), spotlightjs()],
   output: "server",
   adapter: vercel(),
   prefetch: true,
